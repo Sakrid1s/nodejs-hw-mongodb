703d92e49d62b582bd9eb1502235df59b567db4c
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-04 18:34:49 +0300

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,13 +4,19 @@ import { env } from '../utils/env.js';
 import { MONGO_VARS } from '../constants/constants.js';
 
 export const initMongoConnection = async () => {
-  const user = env(MONGO_VARS.MONGODB_USER);
-  const password = env(MONGO_VARS.MONGODB_PASSWORD);
-  const url = env(MONGO_VARS.MONGODB_URL);
-  const dataBase = env(MONGO_VARS.MONGODB_DB, '');
+  try {
+    const user = env(MONGO_VARS.MONGODB_USER);
+    const password = env(MONGO_VARS.MONGODB_PASSWORD);
+    const url = env(MONGO_VARS.MONGODB_URL);
+    const dataBase = env(MONGO_VARS.MONGODB_DB, '');
 
-  await mongoose.connect(
-    `mongodb+srv://${user}:${password}@${url}/${dataBase}?retryWrites=true&w=majority&appName=Cluster-Sakridis`,
-  );
-  console.log('Mongo connection successfully established!');
+    await mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/${dataBase}?retryWrites=true&w=majority&appName=Cluster-Sakridis`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Connection to database error\n', error);
+    process.exit(1);
+    throw error;
+  }
 };
