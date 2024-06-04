9de78c1b9aa4ced8908bfeb7b5ef186fa69dbd3d
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-04 21:42:52 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import './server.js';
+import { setupServer } from './server.js';
+
+const bootstrap = async () => {
+  setupServer();
+};
+
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -2,46 +2,38 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 
-const PORT = 3000;
+import { env } from './utils/env.js';
+import { MONGO_VARS } from './constants/constants.js';
 
-const app = express();
+const PORT = env(MONGO_VARS.PORT);
 
-app.use(express.json());
-app.use(cors());
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+export const setupServer = () => {
+  const app = express();
 
-app.use((req, res, next) => {
-  console.log('firs middleware');
-  next();
-});
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toISOString()}`);
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello' });
-});
-
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
   });
-});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      err: err.message,
+    });
   });
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
