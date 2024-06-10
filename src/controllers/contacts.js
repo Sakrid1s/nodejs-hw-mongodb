a8c701f99a09488d0c8cfb3cfa2da85c82dcb34f
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-10 17:38:30 +0300

    upd

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { getAllContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
 
 export const getAllContactsController = async (req, res) => {
   const allContacts = await getAllContacts();
@@ -10,12 +11,13 @@ export const getAllContactsController = async (req, res) => {
   });
 };
 
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const isInvalidId = !mongoose.Types.ObjectId.isValid(contactId);
   const contact = isInvalidId ? null : await getContactById(contactId);
   if (isInvalidId || !contact) {
-    throw new Error(`No contact was found with id ${contactId}`);
+    next(createHttpError(404, `No contact was found with id ${contactId}`));
+    return;
   }
   return res.status(200).json({
     status: 200,
