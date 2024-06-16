694ad40cff76d96261455aec910fd42e6baa0a61
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-17 00:36:50 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -12,6 +12,9 @@ export const getAllContacts = async ({
   const skip = (page - 1) * perPage;
 
   const contactsQuery = ContactsCollection.find();
+
+  //Another variant
+  /*
   const contactsCount = await ContactsCollection.find()
     .merge(contactsQuery)
     .countDocuments();
@@ -21,6 +24,16 @@ export const getAllContacts = async ({
     .limit(limit)
     .sort({ [sortBy]: sortOrder })
     .exec();
+*/
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
 
   const paginationData = calculatePaginationData(contactsCount, page, perPage);
 
