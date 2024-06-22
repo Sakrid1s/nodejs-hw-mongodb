bd1f2e53d11520f996e3f7bd41e6296d037c9075
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-22 17:35:23 +0300

    upd

---
+++ src/validations/registerUserValidationSchema.js
@@ -0,0 +1,7 @@
+import Joi from 'joi';
+
+export const registerUserValidationSchema = Joi.object({
+  name: Joi.string().required().min(3).max(20),
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6).max(32),
+});
