3086c881d48c011bbfba82daeec680feb192ff4c
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-24 01:16:09 +0300

    Create authenticate.js

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,42 @@
+import createHttpError from 'http-errors';
+import { User } from '../db/models/user.js';
+import { Session } from '../db/models/session.js';
+
+export const authenticate = async (req, res, next) => {
+  const header = req.get('Authorization');
+
+  if (!header) {
+    next(createHttpError(401, 'Unauthorized'));
+    return;
+  }
+
+  const [bearer, token] = header.split(' ');
+
+  if (bearer !== 'Bearer' || !token) {
+    next(createHttpError(401, 'Auth header should be of Bearer type'));
+    return;
+  }
+
+  const session = await Session.findOne({ accessToken: token });
+
+  if (!session) {
+    next(createHttpError(401, 'Session not found'));
+    return;
+  }
+
+  if (Date.now() > session.accessTokenValidUntil) {
+    next(createHttpError(401, 'Session token expired'));
+    return;
+  }
+
+  const user = await User.findById(session.userId);
+
+  if (!user) {
+    next(createHttpError(401, 'Unauthorized'));
+    return;
+  }
+
+  req.user = user;
+
+  return next();
+};
