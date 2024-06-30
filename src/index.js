09d26e2c41324bb1e5966ac1244df4c7fb4b5726
Sakrid1s <bilinets.vitalik@gmail.com>
2024-06-30 15:39:45 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+import { DIR_VARS } from './constants/constants.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { createFolderIfNotExist } from './utils/createFolderIfNotExist.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
+  await createFolderIfNotExist(DIR_VARS.TEMP_UPLOAD_DIR);
   setupServer();
 };
 
