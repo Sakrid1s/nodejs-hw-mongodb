d669ff49e5d8083427c42cfcb38a902c4c011001
Sakrid1s <bilinets.vitalik@gmail.com>
2024-07-08 21:17:45 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import pino from 'pino-http';
 import { DIR_VARS, ENV_VARS } from './constants/constants.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import rootRouter from './routers/index.js';
 import { env } from './utils/env.js';
 
@@ -28,6 +29,7 @@ export const setupServer = () => {
   app.use(rootRouter);
 
   app.use('/uploads', express.static(DIR_VARS.UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
